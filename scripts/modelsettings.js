8a982c68955634b5d588f7193511e004d736ef72
Akinyade Oladapo <michaelintellect2007@gmail.com>
2018-04-14 18:45:37 +0200

    close button
    fix the close button on the portfolio's carousal

--- scripts/modelsettings.js
+++ scripts/modelsettings.js
@@ -110,7 +110,7 @@ var span = document.getElementsByClassName("close")[0];
 		
 	}
 	
-	// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the modal
 span.onclick = function span() {
     modal.style.display = "none";
 	modal1.style.display = "none";
@@ -120,6 +120,7 @@ span.onclick = function span() {
 	modal5.style.display = "none";
 	modal6.style.display = "none";
 };
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function close(event) {
